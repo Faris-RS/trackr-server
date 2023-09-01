793b87cc620cd970a24996e28afe94f348f51d6e
Mohamed Faris <brotofarisrs@gmail.com>
2023-09-01 10:15:05 +0530

    hosting commit

--- routes/admin-routes/admin-user-managment-route.js
+++ routes/admin-routes/admin-user-managment-route.js
@@ -7,7 +7,7 @@ import authMiddleware from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/getAllUsers", authMiddleware, fetchAllUsers);
+router.get("/getAllUsers", fetchAllUsers);
 router.patch("/blockUser", authMiddleware, blockUser);
 
 export default router;
